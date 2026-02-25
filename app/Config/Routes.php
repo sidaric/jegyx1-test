4d60dae8854e2bb99e596fcb1f42484102bc0401
Richard Sinka <thesidaric@gmail.com>
2026-02-25 15:32:16 +0100

    Feat: protect admin routes with auth filter

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/', 'LoginController::index');
+$routes->get('/login', 'LoginController::index');
+$routes->post('/login', 'LoginController::login');
+$routes->get('/logout', 'LoginController::logout');
+$routes->get('/admin', 'LoginController::dashboard', ['filter' => 'auth']);
---
+++ app/Filters/AuthFilter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+
+class AuthFilter implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        if (!session()->get('user_id')) {
+            return redirect()->to('/login')->with('flash_error', 'Kérlek jelentkezz be.');
+        }
+
+        return null;
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        return null;
+    }
+}
